94363a561b2b7518e119b3a8ce08b07eea73cc9c
poornimachennappan <148464475+poornimachennappan@users.noreply.github.com>
2025-02-05 23:06:54 +0530

    code

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+class IfElse 
+{
+	public static void main(String[] args) 
+	{
+		int mark=25;
+		if(mark>35){
+			System.out.println("pass");
+	}
+	    else{
+			System.out.println("fail");
+}
+}
+}
